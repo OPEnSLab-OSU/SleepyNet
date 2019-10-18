0bba99be8a65648ef76ca273d3bee68e9c2f6158
Noah Laptop <noah@koontzs.com>
2019-10-18 16:48:04 -0700

    worked on the tests more, still not finished testing the state

--- simulate/LoomNetworkSimulate/LoomNetworkTest/LoomMACTest.cpp
+++ simulate/LoomNetworkSimulate/LoomNetworkTest/LoomMACTest.cpp
@@ -31,7 +31,6 @@ private:
 	State m_state;
 };
 
-
 // a mock radio class
 class MockRadio : public Radio {
 public:
@@ -53,6 +52,9 @@ public:
 		ON_CALL(*this, wake).WillByDefault([this] { m_radio.wake(); });
 		ON_CALL(*this, send).WillByDefault([this](const Packet& send) { m_radio.send(send); });
 		ON_CALL(*this, recv).WillByDefault([this](TimeInterval& recv) { return m_radio.recv(recv); });
+		// get_state should be called any number of times, as should get_time
+		EXPECT_CALL(*this, get_state).Times(AnyNumber());
+		EXPECT_CALL(*this, get_time).Times(AnyNumber());
 	}
 
 	State get_underlying_state() const { return m_radio.get_state(); }
@@ -61,7 +63,39 @@ private:
 	FakeRadio m_radio;
 };
 
-TEST(MAC, RefreshTimeout) {
+// setup some utility functions to test a sequence of state transitions
+// in the MAC layer
+class MACFixture : public ::testing::Test {
+protected:
+	void SetUp() override {}
+	void TearDown() override {}
+
+	TimeInterval run_until_state_change(MAC& mac, 
+			TimeInterval& time, 
+			const TimeInterval& timeout) const {
+
+		const TimeInterval end_time = time + timeout;
+		const TimeInterval start_time = time;
+		const MAC::State last_state = mac.get_status();
+		EXPECT_TRUE(last_state == MAC::State::MAC_DATA_WAIT || last_state == MAC::State::MAC_REFRESH_WAIT) << "Invalid state for input parameters!";
+		while (mac.get_status() == last_state && time < end_time) {
+			if (last_state == MAC::State::MAC_REFRESH_WAIT)
+				mac.check_for_refresh();
+			else
+				mac.check_for_data();
+			// increment the time by one slot
+			time = time + m_drift.min_drift;
+		}
+		EXPECT_LT(time, end_time);
+		return time - start_time;
+	}
+
+	const Drift m_drift = Drift(TimeInterval(TimeInterval::SECOND, 1), 
+								TimeInterval(TimeInterval::SECOND, 5), 
+								TimeInterval(TimeInterval::SECOND, 10));
+};
+
+TEST_F(MACFixture, RefreshTimeout) {
 	StrictMock<MockRadio> radio;
 	
 	// delegate to a fake state machine
@@ -69,8 +103,7 @@ TEST(MAC, RefreshTimeout) {
 
 	// initialize the MAC (should make no calls to the radio)
 	const Slotter slots(5, 10, 3, 2, 2, 1, SLOT_NONE, 0);
-	const Drift drift(TIME_NONE, TimeInterval(TimeInterval::SECOND, 1), TimeInterval(TimeInterval::SECOND, 1));
-	MAC test_mac(0x1101, DeviceType::END_DEVICE, slots, drift, radio);
+	MAC test_mac(0x1101, DeviceType::END_DEVICE, slots, m_drift, radio);
 	EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_REFRESH_WAIT);
 	EXPECT_EQ(test_mac.get_last_error(), MAC::Error::MAC_OK);
 
@@ -79,7 +112,6 @@ TEST(MAC, RefreshTimeout) {
 		TimeInterval cur_time(TimeInterval::SECOND, 0);
 		EXPECT_CALL(radio, get_time)
 			.WillRepeatedly(ReturnPointee(&cur_time));
-		EXPECT_CALL(radio, get_state).Times(AnyNumber());
 		InSequence seq;
 		EXPECT_CALL(radio, enable).Times(1);
 		EXPECT_CALL(radio, wake).Times(1);
@@ -87,21 +119,17 @@ TEST(MAC, RefreshTimeout) {
 		EXPECT_CALL(radio, send).Times(0);
 		EXPECT_CALL(radio, sleep).Times(1);
 		EXPECT_CALL(radio, disable).Times(1);
-		
+		// run the MAC until it times out
+		const TimeInterval took = run_until_state_change(test_mac, cur_time, m_drift.slot_length * slots.get_total_slots() * 3);
 		// the MAC is supposed to wait exactly one refresh cycle
-		const int refresh_wait_seconds = slots.get_slots_per_refresh() + REFRESH_CYCLE_SLOTS + drift.max_drift.get_time();
-
-		for (int i = 0; i < refresh_wait_seconds; i++) {
-			EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_REFRESH_WAIT);
-			cur_time = TimeInterval(TimeInterval::SECOND, i);
-			test_mac.check_for_refresh();
-		}
+		const TimeInterval refresh_wait = m_drift.slot_length * (slots.get_slots_per_refresh() + REFRESH_CYCLE_SLOTS) + m_drift.max_drift;
 		EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_CLOSED);
+		EXPECT_EQ(took, refresh_wait);
 		EXPECT_EQ(test_mac.get_last_error(), MAC::Error::REFRESH_TIMEOUT);
 	}
 }
-
-TEST(MAC, CoordinatorOperation) {
+// TODO: this doesn't really make sense I think, but I'm not sure how to fix it
+TEST_F(MACFixture, CoordinatorOperation) {
 	StrictMock<MockRadio> radio;
 
 	// delegate to a fake state machine
@@ -109,77 +137,56 @@ TEST(MAC, CoordinatorOperation) {
 
 	// initialize the MAC as a Coordinator (should make no calls to the radio)
 	const Slotter slots(SLOT_NONE, 10, 3, 2, 2, 1, 5, 3);
-	const Drift drift(TIME_NONE, TimeInterval(TimeInterval::SECOND, 1), TimeInterval(TimeInterval::SECOND, 1));
-	MAC test_mac(ADDR_COORD, DeviceType::COORDINATOR, slots, drift, radio);
+	MAC test_mac(ADDR_COORD, DeviceType::COORDINATOR, slots, m_drift, radio);
 	EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_REFRESH_WAIT);
 	EXPECT_EQ(test_mac.get_last_error(), MAC::Error::MAC_OK);
 
-	{
-		// check that the MAC affects the radio exactly how it's supposed to
-		// can get the time or the state, but cannot recieve
-		TimeInterval cur_time(TimeInterval::SECOND, 0);
-		EXPECT_CALL(radio, get_time)
-			.WillRepeatedly(ReturnPointee(&cur_time));
-		EXPECT_CALL(radio, get_state).Times(AnyNumber());
-		EXPECT_CALL(radio, recv).Times(0);
-		// in order: enable -> wake -> send -> sleep
-		EXPECT_CALL(radio, enable).Times(1);
-		for (int loop = 0; loop < 3; loop++) {
-			InSequence seq;
-			EXPECT_CALL(radio, wake).Times(1);
-			const uint8_t next_data_slots = cur_time.get_time() + REFRESH_CYCLE_SLOTS - drift.max_drift.get_time();
-			const uint8_t next_refresh_slots = cur_time.get_time() + slots.get_slots_per_refresh() - drift.max_drift.get_time();
-			Packet expected_packet = RefreshPacket::Factory(ADDR_COORD,
-				TimeInterval(TimeInterval::SECOND, next_data_slots),
-				TimeInterval(TimeInterval::SECOND, next_refresh_slots),
-				0);
-			expected_packet.set_framecheck();
-			EXPECT_CALL(radio, send(expected_packet)).Times(1);
-			EXPECT_CALL(radio, sleep).Times(1);
-			// the MAC should send a refresh packet here
-			{ // make sure timecheck doesn't leak into the outer scope
-				int timecheck = 0;
-				while (++timecheck < 100) {
-					EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_REFRESH_WAIT);
-					test_mac.check_for_refresh();
-					if (test_mac.get_status() != MAC::State::MAC_REFRESH_WAIT) break;
-				}
-				// check for timeout
-				ASSERT_NE(timecheck, 100);
+	// check that the MAC affects the radio exactly how it's supposed to
+	// can get the time or the state, but cannot recieve
+	TimeInterval cur_time(TimeInterval::SECOND, 0);
+	EXPECT_CALL(radio, get_time)
+		.WillRepeatedly(ReturnPointee(&cur_time));
+	EXPECT_CALL(radio, get_state).Times(AnyNumber());
+	EXPECT_CALL(radio, recv).Times(0);
+	// in order: enable -> wake -> send -> sleep
+	EXPECT_CALL(radio, enable).Times(1);
+	for (int loop = 0; loop < 3; loop++) {
+		InSequence seq;
+		EXPECT_CALL(radio, wake).Times(1);
+		const uint8_t next_data_slots = cur_time.get_time() + REFRESH_CYCLE_SLOTS - m_drift.max_drift.get_time();
+		const uint8_t next_refresh_slots = cur_time.get_time() + slots.get_slots_per_refresh() - m_drift.max_drift.get_time();
+		Packet expected_packet = RefreshPacket::Factory(ADDR_COORD,
+			TimeInterval(TimeInterval::SECOND, next_data_slots),
+			TimeInterval(TimeInterval::SECOND, next_refresh_slots),
+			0);
+		expected_packet.set_framecheck();
+		EXPECT_CALL(radio, send(expected_packet)).Times(1);
+		EXPECT_CALL(radio, sleep).Times(1);
+		// the MAC should send a refresh packet here
+		run_until_state_change(test_mac, cur_time, m_drift.slot_length * REFRESH_CYCLE_SLOTS);
+		// for every data cycle
+		for (uint8_t cur_cycle = 0; cur_cycle < slots.get_cycles_per_refresh(); cur_cycle++) {
+			// check the current wake time to make sure it's correct
+			EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_SLEEP_RDY) << "Error: " << static_cast<int>(test_mac.get_last_error());
+			// and will go to sleep until slot #5, where it's supposed to recieve
+			EXPECT_EQ(test_mac.sleep_next_wake_time(), m_drift.slot_length * next_data_slots);
+			// for every device we're supposed to recieve
+			for (uint8_t recv_device = 0; recv_device < slots.get_recv_count(); recv_device++) {
+				// we expect the radio to wake->recv->sleep
+				// calls are still required to be in sequence
+				EXPECT_CALL(radio, wake).Times(1);
+				EXPECT_CALL(radio, recv).Times(AtLeast(1));
+				EXPECT_CALL(radio, sleep).Times(1);
+				// wake the MAC
+				test_mac.sleep_wake_ack();
+				EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_DATA_WAIT);
+				// check for data, advancing the time
+				run_until_state_change(test_mac, cur_time, m_drift.slot_length);
 			}
-			// for every data cycle
-			for (uint8_t cur_cycle = 0; cur_cycle < slots.get_cycles_per_refresh(); cur_cycle++) {
-				// check the current wake time to make sure it's correct
-				EXPECT_EQ(test_mac.get_status(), MAC::State::MAC_SLEEP_RDY) << "Error: " << static_cast<int>(test_mac.get_last_error());
-				// and will go to sleep until slot #5, where it's supposed to recieve
-				EXPECT_EQ(test_mac.sleep_next_wake_time(),
-					TimeInterval(TimeInterval::SECOND,
-					next_data_slots));
-				// for every device we're supposed to recieve
-				for (uint8_t recv_device = 0; recv_device < slots.get_recv_count(); recv_device++) {
-					// we expect the radio to wake->recv->sleep
-					// calls are still required to be in sequence
-					EXPECT_CALL(radio, wake).Times(1);
-					EXPECT_CALL(radio, recv).Times(AtLeast(1));
-					EXPECT_CALL(radio, sleep).Times(1);
-					// wake the MAC
-					test_mac.sleep_wake_ack();
-					// check for data, advancing the time
-					{
-						int timecheck = 0;
-						while (test_mac.get_status() == MAC::State::MAC_DATA_WAIT
-							&& ++timecheck < 100) {
-							test_mac.check_for_data();
-						}
-						ASSERT_EQ(test_mac.get_status(), MAC::State::MAC_SLEEP_RDY);
-						ASSERT_NE(timecheck, 100);
-					}
-				}
-			}
-			
-			// do
-
 		}
-		
+			
+		// TODO: something here
+
 	}
+		
 }
